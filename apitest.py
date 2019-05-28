2c89e76482e10049c44863e1980336eb8dcbd1e7
Alfred Foo <alfredfoo@Alfreds-MacBook-Pro.local>
2019-05-28 23:41:15 +0800

    apitest2.py can now create directory, changed interval time, created test folder BINANCE and compiled csv

--- apitest.py
+++ apitest.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import json
 import pandas as pd 
+import os
 import sys
 import multiprocessing as mp
 
@@ -9,8 +10,9 @@ global parameters
 parameters = {"X-CoinAPI-Key":"8CFD1A47-9381-4B96-BB8E-8E1421DCF137"}
 
 def applydata(listx):
-		response = requests.get("https://rest.coinapi.io/v1/ohlcv/{}/history?period_id=5MIN&time_start=2019-04-26T00:00:00&limit=10000".format(listx),headers=parameters)
+		response = requests.get("https://rest.coinapi.io/v1/ohlcv/{}/history?period_id=1HRS&time_start=2019-04-26T00:00:00&limit=10000".format(listx),headers=parameters)
 		print("{}".format(listx))
+		data=pd.read_csv(json.dumps(response.json()))
 
 def main():
 	response=requests.get("https://rest.coinapi.io/v1/symbols?filter_symbol_id=BINANCE",headers=parameters)
--- apitest2.py
+++ apitest2.py
@@ -3,16 +3,27 @@ import csv
 import json
 import pandas as pd 
 import sys
+import os
 
 parameters = {"X-CoinAPI-Key":"8CFD1A47-9381-4B96-BB8E-8E1421DCF137"}
 
-response=requests.get("https://rest.coinapi.io/v1/symbols?filter_symbol_id=BINANCE",headers=parameters)
+#response=requests.get("https://rest.coinapi.io/v1/symbols?filter_symbol_id=BINANCE",headers=parameters)
+response = requests.get("https://rest.coinapi.io/v1/ohlcv/BINANCE_SPOT_ETH_BTC/history?period_id=1HRS&time_start=2018-05-28T00:00:00&limit=10000",headers=parameters)
 
-data=response.json()
-count=0
+data=pd.read_json(json.dumps(response.json()))
 
-for x in data:
-	print(x["symbol_id"])
-	count+=1
+print(type(data))
 
-print(count)
+codedirectory=os.path.dirname(sys.argv[0])
+
+splittedsymbol="BINANCE_SPOT_ETH_BTC".split("_")[0]
+
+folderpath=os.path.abspath(codedirectory)+"/"+splittedsymbol+"/"
+
+#print(folderpath)
+if not os.path.exists(folderpath):
+	os.mkdir(folderpath)
+
+fullpath=os.path.abspath(codedirectory)+"/"+splittedsymbol+"/"+"BINANCE_SPOT_ETH_BTC"+".csv"
+#print(fullpath)
+data.to_csv(fullpath)
