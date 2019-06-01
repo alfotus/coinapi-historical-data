84794f2a85e45374895beb5d9c7c8ae4ce530b17
Alfred Foo <alfredfoo@Alfreds-MacBook-Pro.local>
2019-06-02 04:44:23 +0800

    created findliquid.py to find LIQUID on coinapi.io due to different name(QUOINE)

---
+++ findliquid.py
@@ -0,0 +1,10 @@
+import requests
+
+hdr={"X-CoinAPI-Key":"09967F63-E8F2-4088-B2DA-FB699902B2F7"}
+
+response=requests.get("https://rest.coinapi.io/v1/exchanges",headers=hdr)
+
+for x in response.json():
+	if "liquid" in x["website"]:
+		print(x["exchange_id"])
+		print(x["website"])
