768911277baadbb45a1167047df5ab27bfb8e8f5
Alfred <alfotus@gmail.com>
2019-06-11 04:25:56 +0800

    For finding quoine, not quoinex

--- findliquid.py
+++ findliquid.py
@@ -1,10 +1,10 @@
 import requests
 
-hdr={"X-CoinAPI-Key":"09967F63-E8F2-4088-B2DA-FB699902B2F7"}
+hdr={"X-CoinAPI-Key":"4DF77F9B-6DBA-4E69-8A13-4126161F48B6"}
 
 response=requests.get("https://rest.coinapi.io/v1/exchanges",headers=hdr)
 
 for x in response.json():
-	if "coinbase" in x["website"]:
+	if "liquid" in x["website"]:
 		print(x["exchange_id"])
 		print(x["website"])
